cf82df5eff488e98ebbc7c881b423d182191bcd6
kliffrog <kliffrog@gmail.com>
2017-11-18 14:00:34 -0600

    Add project files.

---
+++ HireMeCodeFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HireMeCodeFirst.MvcApplication" Language="C#" %>
---
+++ HireMeCodeFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HireMeCodeFirst.Startup))]
+namespace HireMeCodeFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
